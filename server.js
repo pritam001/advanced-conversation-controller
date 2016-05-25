dfef4b7e7d91cfc91a6911efb1233c4e6ed4cf41
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-25 15:46:39 +0530

    Create controlling js

---
+++ server.js
@@ -0,0 +1,32 @@
+/*var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+}) */
+var http = require(‘http’),
+fs = require(‘fs’),
+io = require(‘socket.io’),
+index;
+fs.readFile(‘./chat.html’, function (err, data) {
+ if (err) {
+    throw err;
+ }
+ index = data;
+});
+var server = http.createServer(function(request, response) {
+  response.writeHeader(200, {“Content-Type”: “text/html”});
+  response.write(index);
+  response.end();
+}).listen(1223);
+
+var socket = io.listen(server);
